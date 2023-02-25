3d19448a0ddccf53cca969b8636459d66d628c53
Miranda <93054741+mirandad03@users.noreply.github.com>
2023-02-25 20:15:06 +0000

    testing

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ class BinaryNumber:
         
     def __repr__(self):
         return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
-    
 
-## Implement multiplication functions here. Note that you will have to
-## ensure that x, y are appropriately sized binary vectors for a
-## divide and conquer approach.
+    def __add__(self, other):
+        return BinaryNumber(self.decimal_val + other.decimal_val)
 
 def binary2int(binary_vec): 
     if len(binary_vec) == 0:
@@ -24,37 +22,55 @@ def binary2int(binary_vec):
     return BinaryNumber(int(''.join(binary_vec), 2))
 
 def split_number(vec):
-    return (binary2int(vec[:len(vec)//2]),
-            binary2int(vec[len(vec)//2:]))
+    return [binary2int(vec[:len(vec)//2]),
+            binary2int(vec[len(vec)//2:])]
 
 def bit_shift(number, n):
-    # append n 0s to this number's binary string
     return binary2int(number.binary_vec + ['0'] * n)
     
 def pad(x,y):
-    # pad with leading 0 if x/y have different number of bits
-    # e.g., [1,0] vs [1]
     if len(x) < len(y):
         x = ['0'] * (len(y)-len(x)) + x
     elif len(y) < len(x):
         y = ['0'] * (len(x)-len(y)) + y
-    # pad with leading 0 if not even number of bits
     if len(x) % 2 != 0:
         x = ['0'] + x
         y = ['0'] + y
-    return x,y
+    return x, y
 
 def quadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+    return _quadratic_multiply(x, y).decimal_val
 
+def _quadratic_multiply(x, y):
+    xvec = x.binary_vec
+    yvec = y.binary_vec
+    
+    xvec, yvec = pad(xvec, yvec)
+    
+    if x.decimal_val <= 1 and y.decimal_val <= 1:
+        return BinaryNumber(x.decimal_val * y.decimal_val)
+    
+    x_left, x_right = split_number(xvec) 
+    y_left, y_right = split_number(yvec)
 
-
+    p1 = _quadratic_multiply(x_left, y_left)
+    p2 = _quadratic_multiply(x_left, y_right)
+    p3 = _quadratic_multiply(x_right, y_left)
+    p4 = _quadratic_multiply(x_right, y_right)
+    
+    p1 = bit_shift(p1, 2*(len(xvec)//2))
+    p2 = bit_shift(p2, len(xvec)//2)
+    p3 = bit_shift(p3, len(xvec)//2)
+    
+    return (p1 + p2) + (p3 + p4)
+   
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
-    
+  assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+  a = BinaryNumber(10)
+  b = BinaryNumber(5)
+  c = quadratic_multiply(a, b)
+  print(c) 
     
 def time_multiply(x, y, f):
     start = time.time()
