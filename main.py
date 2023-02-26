bb8871a1ed73c31cbd083c7389c839eb8a7ce668
Miranda <93054741+mirandad03@users.noreply.github.com>
2023-02-26 18:39:57 +0000

    finished

--- main.py
+++ main.py
@@ -48,11 +48,15 @@ def _quadratic_multiply(x, y):
     yvec = y.binary_vec
 
 #Pad xvec and yvec so they are the same length by adding leading 0s if necessary
-    xvec, yvec = pad(xvec, yvec)
+    lenmax = max(len(xvec), len(yvec))
+    xvec = ['0'] * (lenmax - len(xvec)) + xvec
+    yvec = ['0'] * (lenmax - len(yvec)) + yvec
 
 #Base case: If both x and y are ≤ 1, then just return their product.
-    if x.decimal_val <= 1 and y.decimal_val <= 1:
-        return BinaryNumber(x.decimal_val * y.decimal_val)
+    if x.decimal_val == 0 or y.decimal_val == 0:
+        return BinaryNumber(0)
+    if x.decimal_val == 1 and y.decimal_val == 1:
+        return BinaryNumber(1)
 
 #Otherwise, split xvec and yvec into two halves each. Call them x_left x_right y_left y_right
     x_left, x_right = split_number(xvec) 
